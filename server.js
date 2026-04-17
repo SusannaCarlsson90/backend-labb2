a11c1e17be51903835a81705a62bacd88365691e
SusannaCarlsson90 <susanna.carlsson90@icloud.com>
2026-04-17 15:02:33 +0200

    POST klar

--- server.js
+++ server.js
@@ -12,3 +12,38 @@ app.use(cors()); // Tillåt cross-origin
 app.use(express.json()); // Parsa JSON-body
 // 5. Starta servern
 app.listen(5000, () => console.log("Server på port 5000"));
+
+//Routes: 
+//GET (hämta data )
+
+//experiences alla erfarenheter
+app.get("/workexperience", (req, res) => {
+  const experiences = db.prepare(*SELECT FROM workexperience).all();
+  res.json(experiences);
+});
+
+//GET En specifik erfarenhet
+app.get("/workexperience/:id", (req, res) => {
+const experience = db.prepare("SELECT * FROM experiences WHERE id = ?"),get(req.params.id);
+if (!experience) return res.status(404).json({error: "Not found"});
+res.json(experience)
+});
+
+//POST (lägg till data)
+
+app.post("/workexperience", (req, res) => {
+  const { companyname, jobtitle, location, startdate, enddate, description } = req.body; //Data från request bodyn
+  if (!companyname || !jobtitle)
+    return res.status(400).json({message: "companyname och titlename krävs"});
+const stmt = db.prepare(`
+INSERT INTO workexperience (companyname, jobtitle, location, startdate, enddate, description) VALUES (?,?,?,?,?,?)`);
+
+try {const result = stmt.run(companyname, jobtitle, location, startdate, enddate, description);
+res.status(201).json({id: result.lastInsertRowid, ...req.body});
+} catch (error) {
+ res.status(500).json({message: "Could not insert experience"});
+ }
+}); 
+
+
+
